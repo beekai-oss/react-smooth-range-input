dcd9c89866b4a963fc396523d1351a647756fe3d
bill <bluebill1049@hotmail.com>
2019-01-15 22:25:43 +1100

    function rename

--- src/utilities/debounce.test.ts
+++ src/utilities/debounce.test.ts
@@ -3,11 +3,11 @@ import debounce from './debounce';
 describe('debounce', () => {
   it('should debounced function invoke', () => {
     const testFunction = jest.fn();
-    const what = debounce(testFunction, 2000);
-    what();
-    what();
-    what();
-    what();
+    const debounceFunction = debounce(testFunction, 2000);
+    debounceFunction();
+    debounceFunction();
+    debounceFunction();
+    debounceFunction();
     expect(testFunction.mock.calls.length).toBe(1);
   });
 });
