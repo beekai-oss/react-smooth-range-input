8b4fd9bba8641bfbbd91cda240cebfa23db15689
bill <bluebill1049@hotmail.com>
2019-01-15 15:19:47 +1100

    update snapshot and tsconfig

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   globals: {
     'ts-jest': {
-      tsConfigFile: 'tsconfig.jest.json',
+      tsConfig: 'tsconfig.jest.json',
     },
   },
   testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
